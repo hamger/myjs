cfb5b0ef2243075d580f47af40508505958b557a
Hanger <2313303800@qq.com>
2018-01-24 08:50:27 +0800

    del

--- server/outPut.js
+++
@@ -1,8 +0,0 @@
-module.exports =  function (response, respObj) {
-    response.writeHead(200,{
-    	'Content-type':'application/json',
-    	"Access-Control-Allow-Origin":"http://localhost:8080"
-	})
-    response.write(respObj)
-    response.end()
-}
--- src/vuex/getters.js
+++
@@ -1,6 +0,0 @@
-export const getArticles = (state) => state.articles;
-export const getTopics = (state) => state.topics;
-export const getShow = (state) => state.show;
-export const getShow_2 = (state) => state.show_2;
-export const getBonusTexts = (state) => state.texts;
-export const getLoginWay=(state)=> state.loginway;
